fd5c20aae2142155f9b291e0d8c23299b4444506
Weksi Budiaji <budiaji@untirta.ac.id>
2021-05-08 14:30:02 +0000

    version 0.0.3

--- inst/doc/ddp.R
+++ inst/doc/ddp.R
@@ -9,3 +9,8 @@ kalori(simulasi)
 skorpph(simulasi, wilayah = "Banten")
 skorpph(simulasi, wilayah = "Maluku")
 
+## -----------------------------------------------------------------------------
+set.seed(1)
+dat <- matrix(sample(1:7,10*5, replace = TRUE), 10,5)
+valid(dat)
+
